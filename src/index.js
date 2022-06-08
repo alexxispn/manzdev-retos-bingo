cd8fdc4cff120bfb5fdac465c916249c3232bc6e
“Alexis <alexisplettener@gmail.com>
2022-06-08 13:04:52 +0100

    Css & html

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const randomNumber = () => {
+    Math.random()
+}
+
+const playButton = document.querySelector(".play");
+playButton.addEventListener("click", console.log("hola mundo"));
+
+randomNumber(90)
